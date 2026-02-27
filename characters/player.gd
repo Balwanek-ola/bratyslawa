e7ff0614d4d2ce8cd789d0bb2a9c88a145d97183
szmatolot <sopelameliaa@gmail.com>
2026-02-27 21:54:21 +0100

    asasfasfa

--- characters/player.gd
+++ characters/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 500.0
 var stop = false
 
 func _physics_process(delta: float) -> void:
--- scenes/game.gd
+++ scenes/game.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 @onready var player = $Player
 
+var positions = [Vector2(-100, 200), Vector2(-100, 450), Vector2(-100, 600), 
+Vector2(1610, 200), Vector2(1610, 450), Vector2(1610, 600)]
+
 func _ready() -> void:
 	play_cg()
 
