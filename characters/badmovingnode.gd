a47665fbb13d5ca9faec4f532946da47a5247deb
szmatolot <sopelameliaa@gmail.com>
2026-02-27 22:40:01 +0100

    b

--- characters/badmovingnode.gd
+++ characters/badmovingnode.gd
@@ -10,3 +10,12 @@ func _physics_process(delta: float) -> void:
 	if stop == false and direction != null:
 		velocity = SPEED * Vector2(direction, 0)
 		self.position += velocity
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		body.hit()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
--- characters/player.gd
+++ characters/player.gd
@@ -9,3 +9,7 @@ func _physics_process(delta: float) -> void:
 		var input_vector := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 		velocity = input_vector * SPEED
 		move_and_slide()
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	pass # Replace with function body.
