2a3ced11709680a358d4e79d3a4d127cb5bed0f6
szmatolot <sopelameliaa@gmail.com>
2026-02-27 23:59:46 +0100

    jkhitggfuyddsfsghj

--- characters/player.gd
+++ characters/player.gd
@@ -8,10 +8,17 @@ func _physics_process(delta: float) -> void:
 	if stop == false:
 		var input_vector := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 		velocity = input_vector * SPEED
+#		if input_vector.y == 1:
+#			velocity*= 1.5
+#		elif input_vector.y -- -1:
+#			velocity*= 0.5
 		move_and_slide()
 
 func hit():
 	die()
 	
 func die():
-	print("died")
+	var gamescene = get_parent()
+	if gamescene != null and gamescene.name == "Game":
+		gamescene.die()
+	
--- scenes/game.gd
+++ scenes/game.gd
@@ -8,15 +8,24 @@ var game = true
 #@onready var badthing = preload(res://characters/badmoving.tscn)
 @export var badthing: PackedScene
 var positions = [Vector2(-100, 200), Vector2(-100, 450), Vector2(-100, 600), 
-Vector2(1610, 200), Vector2(1610, 450), Vector2(1610, 600)]
+Vector2(1700, 200), Vector2(1700, 450), Vector2(1700, 600)]
 
 func _ready() -> void:
-	play_cg()
-	startspawning()
+	retry()
 
 
 func _process(delta: float) -> void:
 	pass
+
+func retry():
+	get_tree().paused = false
+	$Death.visible = false
+	play_cg()
+	startspawning()
+	var fish = $fish.get_children()
+	if fish != null:
+		for i in range(fish.size()):
+			fish[i].queue_free()
 
 func play_cg():
 	player.stop = true
@@ -27,14 +36,25 @@ func play_cg():
 func spawnthings():
 	var r = randi_range(0, 5)
 	var newthing = badthing.instantiate()
-	add_child(newthing)
+	$fish.add_child(newthing)
 	newthing.position = positions[r]
 	if r < 3:
 		newthing.direction = 1
 	else:
 		newthing.direction = -1
+	if newthing.position.x > 1700 or newthing.position.x < -100:
+		newthing.queue_free()
 
 func startspawning():
 	while game:
 		spawnthings()
 		await get_tree().create_timer(2).timeout
+
+func die():
+	$Death.visible = true
+	get_tree().paused = true
+
+
+func _on_retry_pressed() -> void:
+	print(retry)
+	retry()
