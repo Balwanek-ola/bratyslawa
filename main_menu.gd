617b20c5d9a8097395851d6377cd3e95b8f847f3
Yukka <yukka.spoko@gmail.com>
2026-02-28 13:30:35 +0100

    ebebebe

--- main_menu.gd
+++ main_menu.gd
@@ -3,13 +3,13 @@ extends Node2D
 @onready var mainbuttons: VBoxContainer = $Mainbuttons
 @onready var options: Panel = $Options
 @onready var Tutorial2: Panel = $Tutorial2
-
+@onready var Credits: Panel = $Options/Credits2/Credits
 
 func _ready():
 	mainbuttons.visible = true
 	options.visible = false
 	Tutorial2.visible = false
-
+	Credits.visible = false
 
 func _on_start_pressed() -> void:
 	$Mainbuttons/Button_Manager/Start/AudioStreamPlayer.play()
@@ -22,7 +22,7 @@ func _on_options_pressed() -> void:
 
 	mainbuttons.visible = false
 	options.visible = true 
-
+	
 
 func _on_quit_pressed() -> void:
 	$Mainbuttons/Button_Manager/Start/AudioStreamPlayer.play()
@@ -54,3 +54,14 @@ func _on_button_pressed() -> void:
 
 
 	
+
+
+func _on_credits2_pressed() -> void:
+	mainbuttons.visible = false
+	Tutorial2.visible = false
+	options.visible = false
+	Credits.visible = true
+
+
+func _on_credits_2_pressed() -> void:
+	pass # Replace with function body.
