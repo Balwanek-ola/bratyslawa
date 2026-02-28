eb4be6bd790de6fe794523fe26e1b57e291edeaf
szmatolot <sopelameliaa@gmail.com>
2026-02-28 02:54:42 +0100

    ssssssdffweqsdfdedbfddbgfd

--- scenes/game.gd
+++ scenes/game.gd
@@ -3,16 +3,18 @@ extends Node2D
 var game = true
 
 var fishcount: int = 0
+var depth: int = 0
 
 @onready var player = $Player
 
 #@onready var badthing = preload(res://characters/badmoving.tscn)
 @export var badthing: PackedScene
 var lastY = 400
-var positions = [Vector2(-100, 400), Vector2(-100, 650), Vector2(-100, 800), 
-Vector2(1700, 400), Vector2(1700, 650), Vector2(1700, 800)]
+var positions: Array
 
 func _ready() -> void:
+	generate_pos()
+	depthcount()
 	retry()
 
 
@@ -20,54 +22,70 @@ func _process(delta: float) -> void:
 	var fish = $fish.get_children()
 	if fish != null:
 		for i in range(fish.size()):
-			fish[i].position.y -= 100 * delta
+			fish[i].position.y -= 150 * delta
 			if fish[i].position.x > 1800 or fish[i].position.x < -200:
 				fish[i].queue_free()
 				fishcount -= 1
 
 func retry():
 	get_tree().paused = false
+	game = true
+	depth = 0
 	lastY = 250
 	$Death.visible = false
 	play_cg()
-	startspawning()
+	
 	var fish = $fish.get_children()
 	if fish != null:
 		for i in range(fish.size()):
 			fish[i].queue_free()
 			fishcount -= 1
 
+func generate_pos():
+	var a = 350
+	for i in range(10):
+		positions.append(a)
+		a += 200
+
 func play_cg():
 	player.stop = true
 	$entry.play("fallin")
 	await get_tree().create_timer(2.1).timeout
 	player.stop = false
+	startspawning()
 
 func spawnthings():
-	var r = randi_range(0, 5)
+	var r = randi_range(0, 9)
+	var ry = randi_range(0, 1)
 	var newthing = badthing.instantiate()
 	$fish.add_child(newthing)
 	fishcount += 1
-	newthing.position = positions[r]
-	if r < 3:
+	newthing.position.y = positions[r]
+	if ry == 0:
 		newthing.direction = 1
-#		newthing.position = Vector2(-100, lastY) 
-#		lastY =+ randi_range(150, 300)
+		newthing.position.x = -100
 	else:
 		newthing.direction = -1
-#		newthing.position = Vector2(1700, lastY) 
-#		lastY =+ randi_range(600, 800)
+		newthing.position.x = 1700
 
 func startspawning():
 	while game:
-		if fishcount < 9:
+		if fishcount < 100:
 			spawnthings()
-		await get_tree().create_timer(1.5).timeout
+		await get_tree().create_timer(0.2).timeout
 
 func die():
 	$Death.visible = true
 	get_tree().paused = true
+	game = false
 
 
 func _on_retry_pressed() -> void:
 	retry()
+
+func depthcount():
+	while game:
+		await get_tree().create_timer(0.8).timeout
+		depth += 1
+		$UI/VBoxContainer/Label.text = "depth: %s" %depth
+		
