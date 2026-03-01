1ab331addd4de52f4505756c01711af5768f8e89
szmatolot <sopelameliaa@gmail.com>
2026-03-01 07:48:59 +0100

    polish

--- intro.gd
+++ intro.gd
@@ -5,7 +5,6 @@ extends Node2D
 func _ready() -> void:
 	$CanvasLayer/AnimationPlayer.play("fade_in")
 	await get_tree().create_timer(6).timeout
-	$AudioStreamPlayer.play()
 	$CanvasLayer/AnimationPlayer.play("fade_out")
 	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://Main Menu.tscn")
