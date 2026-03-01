a8bfe278202f3f1fe0f449eaa23a0223c07d58a9
Yukka <yukka.spoko@gmail.com>
2026-03-01 04:09:49 +0100

    sdsdsd

--- sfx.gd
+++ sfx.gd
@@ -1,11 +1,11 @@
 extends HSlider
-
 @export var audio_bus_name: String
 var audio_bus_id
 func _ready():
 	AudioServer.get_bus_index(audio_bus_name)
 	
 
-func _on_h_slider_value_changed(value: float) -> void:
+func _on_value_changed(value: float ):
+
 	var db = linear_to_db(value)
 	AudioServer.set_bus_volume_db(audio_bus_id,db)
